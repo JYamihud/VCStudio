fee8dda36394e6d842bf9085e9472a4a3150ddb0
JYamihud <blenderdumbass@gmail.com>
2020-11-11 20:58:15 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 # THIS FILE IS A PART OF VCStudio
 # PYTHON 3
 
-# Importing various things
 
+# Testing the software
 from troubleshooter import troubleshooter
 
+# Importing various things
+from settings import settings
+from settings import talk
 
+talk.allert(talk.text("imissedyouwelcommessage"))
