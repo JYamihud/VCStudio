ba237d1f0a5dc8b2ec1710596dd7e1068bef3d35
JYamihud <blenderdumbass@gmail.com>
2020-11-14 23:06:15 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # THIS FILE IS A PART OF VCStudio
 # PYTHON 3
 
+import sys
 
 # Testing the software
 from troubleshooter import troubleshooter
@@ -9,8 +10,19 @@ from troubleshooter import troubleshooter
 from settings import settings
 from settings import talk
 from project_manager import pm_console
+from project_manager import pm_gtk
 
+# The :) I'm Happy To See You
 talk.alert(talk.text("imissedyouwelcommessage"))
 
+# If any arguments
+console_force = False
+if len(sys.argv) > 1 and sys.argv[1] == "-c":
+    console_force = True
+
+
 # Project Manager console version
-pm_console.run()
+if not settings.read("Python-is-good") or console_force:
+    pm_console.run()
+else:
+    pm_gtk.run()
