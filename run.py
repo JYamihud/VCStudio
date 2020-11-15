39509d3346cdefd6b05f8ca616149246b204f77a
JYamihud <blenderdumbass@gmail.com>
2020-11-15 18:26:24 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from troubleshooter import troubleshooter
 from settings import settings
 from settings import talk
 from project_manager import pm_console
-from project_manager import pm_gtk
+
 
 # The :) I'm Happy To See You
 talk.alert(talk.text("imissedyouwelcommessage"))
@@ -25,4 +25,6 @@ if len(sys.argv) > 1 and sys.argv[1] == "-c":
 if not settings.read("Python-is-good") or console_force:
     pm_console.run()
 else:
+    # I'm importing it here so it wont give problems on non GTK systems.
+    from project_manager import pm_gtk
     pm_gtk.run()
