78ece5f98a22cafb5a1bb8282d730606094ee5f2
JYamihud <blenderdumbass@gmail.com>
2020-11-17 07:54:00 +0200

    Add files via upload

--- settings/settings.py
+++ settings/settings.py
@@ -11,12 +11,14 @@ def read(setting):
     #finding the keyword
     for line in data:
         if line.startswith(setting):
-            return line.replace(setting+" = ", "")
+            return convert(line.replace(setting+" = ", ""))
     
     return False
 
 def write(setting, value):
     
+    # Making sure that the value is string'
+    value = str(value)
     
     #opening the file
     data = open("settings/settings.data")
@@ -56,3 +58,47 @@ def list_languages():
     all_langs = sorted(r)
     
     return all_langs
+
+def load_all():
+    
+    # This function will preload everything for the settings file into the RAM
+    # so if something has to be checked on every frame. I would not need to deal
+    # with it constantly. But rather have a ddictionary in the RAM to which I
+    # am going to refer at each frame / instance etc.
+    
+    ret = {}
+    
+    # Opening the file.
+    data = open("settings/settings.data")
+    data = data.read()
+    data = data.split("\n")
+    
+    # Parsing the file.
+    for d in data:
+        if d:
+            ret[d[:d.find(" = ")]] = convert(d[d.find(" = ")+3:])
+    
+    # Returning
+    return ret
+
+def convert(string):
+    
+    # This function will convert a string of value. Into the value it self.
+    # For exmple if it has float or boolean (True, False, None) data in the
+    # settings file. So it's gonna be easier to parse later on. 
+    
+    # Trying fload
+    try:
+        string = float(string)
+    except:
+        
+        # Trying boolean
+        if string == "True":
+            string = True
+        elif string == "False":
+            string = False
+        elif string == "None":
+            string = None
+    # That's it
+    return string
+        
